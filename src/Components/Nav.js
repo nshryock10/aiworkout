da51303e87bf425e8373e2904713d5ad20c430a5
Nick Shryock <nickshryock@Nicks-Air.lan>
2023-07-24 21:41:25 -0400

    added logos

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,10 @@
-
+import fiit_logo from '../FIIT tagline.png';
+import "./Nav.css"
 
 function Nav() {
     return (
-      <div >
-        <h1>Fiit.io</h1>
+      <div className="nav-container">
+        <img src={fiit_logo} alt="fiit logo"></img>
       </div>
     );
   }
