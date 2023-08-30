811bb1ba94727ae608bbc8e999de23bd9cdf774e
Nick Shryock <nickshryock@Nicks-Air.lan>
2023-08-30 10:19:51 -0400

    changing path dir

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'src', 'build')));
 
 app.use('/', router)
 
