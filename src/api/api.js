0fca35cdb2bf15520b13897a76f6db1c050e7f5b
Nick Shryock <nickshryock@Nicks-Air.lan>
2023-08-30 13:23:39 -0400

    changing path dir2

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,10 @@
 export const API_ENDPOINT = "http://localhost:3000"; //Uncomment in Prod
 export const AWS_ENDPOINT = "https://a8jukcsjq9.execute-api.us-east-1.amazonaws.com/dev";
+export const HER_ENDPOINT = "https://fiit-8a6ab7670425.herokuapp.com"
 
 export const getWorkout = async (prompt) => {
     try{
-    const response = await fetch(`${AWS_ENDPOINT}/chat`, {
+    const response = await fetch(`${HER_ENDPOINT}/chat`, {
         method: "POST",
         body: JSON.stringify({
             message: prompt
